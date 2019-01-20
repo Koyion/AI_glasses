835b5fcac7c181cd726b0f073d5ee9a250705a85
Koyion <Shokasd@ya.ru>
2019-01-20 17:32:44 +0300

    initial model try, next fix errors

--- Glass_predict/model.py
+++ Glass_predict/model.py
@@ -8,8 +8,61 @@ from scipy import ndimage
 import tensorflow as tf
 from tensorflow.python.framework import ops
 from nn_functions import *
+from model_functions import *
+
+X_train_orig, Y_train, X_test_orig, Y_test, classes = load_dataset()
 
 
-X_train_orig, Y_train_orig, X_test_orig, Y_test_orig, classes = load_dataset()
+X_train = X_train_orig/255.
+X_test = X_test_orig/255.
+print ("number of training examples = " + str(X_train.shape[0]))
+print ("number of test examples = " + str(X_test.shape[0]))
+print ("X_train shape: " + str(X_train.shape))
+print ("Y_train shape: " + str(Y_train.shape))
+print ("X_test shape: " + str(X_test.shape))
+print ("Y_test shape: " + str(Y_test.shape))
+conv_layers = {}
+
+X, Y = create_placeholders(64, 64, 3, 6)
+print ("X = " + str(X))
+print ("Y = " + str(Y))
+
+print(str(Y_train[0]))
+print(str(Y_train.shape))
+
+tf.reset_default_graph()
+with tf.Session() as sess_test:
+    parameters = initialize_parameters()
+    init = tf.global_variables_initializer()
+    sess_test.run(init)
+    print("W1 = " + str(parameters["W1"].eval()[1,1,1]))
+    print("W2 = " + str(parameters["W2"].eval()[1,1,1]))
 
 
+tf.reset_default_graph()
+
+with tf.Session() as sess:
+    np.random.seed(1)
+    X, Y = create_placeholders(64, 64, 3, 6)
+    parameters = initialize_parameters()
+    Z3 = forward_propagation(X, parameters)
+    init = tf.global_variables_initializer()
+    sess.run(init)
+    a = sess.run(Z3, {X: np.random.randn(2,64,64,3), Y: np.random.randn(2,6)})
+    print("Z3 = " + str(a))
+
+tf.reset_default_graph()
+
+with tf.Session() as sess:
+    np.random.seed(1)
+    X, Y = create_placeholders(64, 64, 3, 6)
+    parameters = initialize_parameters()
+    Z3 = forward_propagation(X, parameters)
+    cost = compute_cost(Z3, Y)
+    init = tf.global_variables_initializer()
+    sess.run(init)
+    a = sess.run(cost, {X: np.random.randn(4,64,64,3), Y: np.random.randn(4,6)})
+    print("cost = " + str(a))
+
+
+_, _, parameters = model(X_train, Y_train, X_test, Y_test)
