f7a9e63c2fe144795773b267baa5539d45fd4645
Koyion <Shokasd@ya.ru>
2019-01-20 17:10:42 +0300

    initial installs and load function

---
+++ Glass_predict/model.py
@@ -0,0 +1,15 @@
+import math
+import numpy as np
+import h5py
+import matplotlib.pyplot as plt
+import scipy
+from PIL import Image
+from scipy import ndimage
+import tensorflow as tf
+from tensorflow.python.framework import ops
+from nn_functions import *
+
+
+X_train_orig, Y_train_orig, X_test_orig, Y_test_orig, classes = load_dataset()
+
+
